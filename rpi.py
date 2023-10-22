e64f73aa8ad318adca8e89d8484b20ce2cc302d2
simburto <tingseamus@gmail.com>
2023-10-22 17:02:31 -0400

    finished

--- rpi.py
+++ rpi.py
@@ -3,11 +3,17 @@ import network
 from utime import sleep
 import re
 from machine import Pin
+from servo import Servo
 
 led = Pin(25, Pin.OUT)
 rp2.country('CA')
-ssid = 'sSweet24'
-password = 'abcdef123456!!'
+ssid = 'ssid here'
+password = 'passwrod here'
+
+hpos = 90
+vpos = 90
+vservo = Servo(pin_id=16)
+hservo = Servo(pin_id=15)
 
 def connect():
     #Connect to WLAN
@@ -33,11 +39,11 @@ connection.listen(0)
 conn, addr = connection.accept()
 connection.setblocking(False)
 result = [0]
+vservo.write(90)
+hservo.write(90)
 try:
     while True:
         FIRE = False
-        print('receiving')
-        print(connection)
         data = conn.recv(6)
         decode = data.decode()
         print(decode)
@@ -45,14 +51,20 @@ try:
         if decode[0] == 'c':
             FIRE = True
             print("FIRE")
-        elif decode[0] == 'h':
+        elif decode[0] == 'v':
             result = decode[1:]
             horizontal_angle = float(result)
             print(horizontal_angle)
-        elif decode[0] == 'v':
+            hpos = hpos + horizontal_angle/5
+            if hpos < 200:
+                hservo.write(hpos)
+        elif decode[0] == 'h':
             result = decode[1:]
             vertical_angle = float(result)
             print(vertical_angle)
+            vpos = vpos + vertical_angle/5
+            if vpos < 200:
+                vservo.write(vpos)
 except KeyboardInterrupt:
     connection.close()
     machine.reset()
